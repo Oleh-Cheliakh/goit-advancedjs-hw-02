9b310c7a11fd0147758eed957fc2727cbfcf8149
Oleh Cheliakh <oleh.cheliakh@gmail.com>
2024-01-08 20:19:59 +0200

    Add promise creator

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,12 +3,17 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+//Selects button and timer elements
 const buttonStart = document.querySelector('button[data-start]');
 const daysNumber = document.querySelector('span[data-days]');
 const hoursNumber = document.querySelector('span[data-hours]');
 const minutesNumber = document.querySelector('span[data-minutes]');
 const secondsNumber = document.querySelector('span[data-seconds]');
+
+//Creates variable in order to store picked data and interval ID
 let pickedDate, intervalId;
+
+//Variable stores showed time
 let dataShowed = {
   days: 0,
   hours: 0,
@@ -16,18 +21,23 @@ let dataShowed = {
   seconds: 0,
 };
 
+//Sets button default condition
 buttonStart.disabled = true;
 
+//Parameters for data picker
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    //Checks if selected date bigger than current date
     if (selectedDates[0] <= new Date()) {
+      //Changes button condition to active
       if (!buttonStart.disabled) {
         buttonStart.disabled = true;
       }
+      //Show custom warning message if wrong data was picked
       return iziToast.show({
         title: 'Warning',
         message: 'Please choose a date in the future',
@@ -35,13 +45,16 @@ const options = {
         position: 'topRight',
       });
     }
+    //Disables button after timer start
     buttonStart.disabled = false;
     pickedDate = selectedDates[0];
   },
 };
 
+//Invokes custom data picker
 flatpickr('#datetime-picker', options);
 
+//Converts data from picker in object
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -61,17 +74,22 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+//Adds zero at the beginning if number has only 1 digit
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
 
+//Show time left in html elements
 function showTimeLeft() {
   const currentDate = new Date();
   const leftTime = pickedDate - currentDate;
+  //Stops countind if timer reached zero
   if (leftTime < 1) {
     return clearInterval(intervalId);
   }
   const { days, hours, minutes, seconds } = convertMs(leftTime);
+
+  //Changes time in element only if it's not equal to currently showing time
   if (days != dataShowed.days) {
     daysNumber.textContent = addLeadingZero(days);
     dataShowed.days = days;
@@ -90,6 +108,7 @@ function showTimeLeft() {
   }
 }
 
+//Starts time count down on button start click
 buttonStart.addEventListener('click', event => {
   buttonStart.disabled = true;
   intervalId = setInterval(showTimeLeft, 1000);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,64 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+//Selects form and input elements
+const delayInput = document.querySelector('input[name=delay]');
+const stepInput = document.querySelector('input[name=step]');
+const amountInput = document.querySelector('input[name=amount]');
+const formElement = document.querySelector('.form');
+
+//Create promise with random status and delay
 function createPromise(position, delay) {
+  //Generates random status
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  //Creates promise which return position and delay parameters
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfills and pass values
+        resolve({ position, delay });
+      } else {
+        // Rejects and pass values
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
+
+//Create promises on form submit
+formElement.addEventListener('submit', event => {
+  //Prevent page reloading
+  event.preventDefault();
+
+  //Variables store data from inputs
+  let delay = parseInt(delayInput.value);
+  let promiseAmount = parseInt(amountInput.value);
+
+  //Creates number of promises equal to Amount field
+  for (let promiseNumber = 1; promiseNumber <= promiseAmount; promiseNumber++) {
+    createPromise(promiseNumber, delay)
+      .then(({ position, delay }) => {
+        //Show custom green notification
+        iziToast.show({
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+          color: 'green',
+          position: 'topRight',
+          transitionIn: 'fadeInDown',
+        });
+      })
+      .catch(({ position, delay }) => {
+        //Show custom red notification
+        iziToast.show({
+          message: `❌ Rejected promise ${position} in ${delay}ms`,
+          color: 'red',
+          position: 'topRight',
+          transitionIn: 'fadeInDown',
+        });
+      });
+
+    //Increase delay by amount sat up in Step input
+    delay += parseInt(stepInput.value);
+  }
+});
